2ee45b694c7525a2c820f419168e6f48c1edaa59
Baker, Kevin <Kevin.Baker@analog.com>
2022-05-26 12:50:39 -0400

    switching to git commands

--- cmd/git-mirror/main.go
+++ cmd/git-mirror/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Git Mirror Command")
+
+	const GIT_DEST_PATH string = "/Users/kbaker/Code/Analog/vlab/apps/archive"
+
+	dirList := []string{
+		"vlab-app-magnetics",
+		"vlab-app-service-magnetics",
+	}
+
+	cmd := "echo"
+	flags := ""
+
+	for _, dir := range dirList {
+		cmdfull := exec.Command(cmd, flags, dir)
+		stdout, err := cmdfull.Output()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Println("------------------------------------------")
+		fmt.Printf("%s %s", cmd, dir)
+		fmt.Println("")
+		fmt.Print(string(stdout))
+
+	}
+
 }
