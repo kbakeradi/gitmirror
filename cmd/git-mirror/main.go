45ebecacba2101ba5b396c9cdc8a258d249a91a8
Baker, Kevin <Kevin.Baker@analog.com>
2022-05-26 18:10:38 -0400

    added design system

--- cmd/git-mirror/main.go
+++ cmd/git-mirror/main.go
@@ -20,17 +20,49 @@ func main() {
 	fmt.Println(" ")
 	// Settings
 	gitList := []gitData{
-		{
-			name: "vlab-app-magnetics",
-			src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-app-magnetics/vlab-app-magnetics",
-			dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-app-magnetics.git",
-		},
-		{
-			name: "vlab-app-service-magnetics",
-			src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-app-service-magnetics/vlab-app-service-magnetics",
-			dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-app-service-magnetics.git",
-		},
+		// {
+		// 	name: "vlab-app-magnetics",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-app-magnetics/vlab-app-magnetics",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-app-magnetics.git",
+		// },
+		// {
+		// 	name: "vlab-app-service-magnetics",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-app-service-magnetics/vlab-app-service-magnetics",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-app-service-magnetics.git",
+		// },
+		// {
+		// 	name: "vlab-appkit-react",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-app-template-react",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-appkit-react.git",
+		// },
+		// {
+		// 	name: "vlab-appkit-react",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-appkit-react",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-appkit-react.git",
+		// },
+		// {
+		// 	name: "vlab-build-tools",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-build-tools",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-build-tools.git",
+		// },
+		// {
+		// 	name: "vlab-core-components",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-core-components",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-core-components.git",
+		// },
+		// {
+		// 	name: "vlab-form-builder",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-form-builder",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-form-builder.git",
+		// },
+		// {
+		// 	name: "vlab-form-builder-next",
+		// 	src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-form-builder-next",
+		// 	dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-form-builder-next.git",
+		// },
 	}
+
+	// git@ssh.dev.azure.com:v3/adi-vlab/vlab-design-system/vlab-build-tools
 
 	const GIT_LOCAL_PATH = "/Users/kbaker/Code/Analog/vlab/apps/archive"
 
