84a0cc8873fdfae1b0dc8850768854fd3e1233f6
Baker, Kevin <Kevin.Baker@analog.com>
2022-05-26 16:33:19 -0400

    working

--- cmd/git-mirror/main.go
+++ cmd/git-mirror/main.go
@@ -2,35 +2,56 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
+	"strings"
 )
 
+type gitData struct {
+	name string
+	src  string
+	dest string
+}
+
+func writeCmd(cmd string, args []string) {
+	fmt.Printf("%s %s\n", cmd, strings.Join(args, " "))
+}
 func main() {
 	fmt.Println("Git Mirror Command")
 
-	const GIT_DEST_PATH string = "/Users/kbaker/Code/Analog/vlab/apps/archive"
-
-	dirList := []string{
-		"vlab-app-magnetics",
-		"vlab-app-service-magnetics",
+	// Settings
+	gitList := []gitData{
+		{
+			name: "vlab-app-magnetics",
+			src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-app-magnetics/vlab-app-magnetics",
+			dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-app-magnetics.git",
+		},
+		{
+			name: "vlab-app-service-magnetics",
+			src:  "git@ssh.dev.azure.com:v3/adi-vlab/vlab-app-service-magnetics/vlab-app-service-magnetics",
+			dest: "ssh://git@bitbucket.analog.com:7999/vlabarchive/vlab-app-service-magnetics.git",
+		},
 	}
 
-	cmd := "echo"
-	flags := ""
+	const GIT_LOCAL_PATH = "/Users/kbaker/Code/Analog/vlab/apps/archive"
 
-	for _, dir := range dirList {
-		cmdfull := exec.Command(cmd, flags, dir)
-		stdout, err := cmdfull.Output()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("----------- COPY AND RUN THE BELOW CODE ------------------")
+	fmt.Println("")
+	fmt.Println("")
 
-		fmt.Println("------------------------------------------")
-		fmt.Printf("%s %s", cmd, dir)
+	for _, git := range gitList {
+		workingDir := GIT_LOCAL_PATH + "/" + git.name
+		writeCmd("git", []string{"clone", git.src, workingDir})
+		writeCmd("cd", []string{workingDir})
+		writeCmd("git", []string{"remote", "add", "bitbucket", git.dest})
+		writeCmd("git", []string{"branch", "-M", "main"})
+		writeCmd("git", []string{"push", "bitbucket", "main"})
+		writeCmd("cd", []string{"-"})
 		fmt.Println("")
-		fmt.Print(string(stdout))
-
+		fmt.Println("")
 	}
-
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("----------- COPY AND RUN THE ABOVE CODE ------------------")
 }
